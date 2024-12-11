7c71603c00f508c2116d4b80c000323c99a2835a
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-12-11 16:31:29 +0500

    Pushed kouy

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -67,7 +67,7 @@ const Sales = () => {
 
     return (
         <>
-            <div className="bg-white dark:bg-gray-800 dark:text-gray-200 shadow-lg rounded-lg p-6 relative">
+            <div className="bg-white dark:bg-gray-800 dark:text-gray-200 shadow-lg rounded-lg p-6 relative ">
                 <div className="mb-4 flex justify-between flex-row gap-5 flex-wrap">
                     <h2 className="text-2xl font-bold mb-4 text-gray-800 dark:text-gray-100">
                         Sotuvlar
@@ -85,7 +85,7 @@ const Sales = () => {
 
                         {/* Conditional Rendered Filters and Actions */}
                         {showFilters && (
-                            <div className="flex flex-col    items-end gap-4 absolute dark:bg-gray-800 bg-white  w-[290px] right-5 p-6 pt-0 rounded-lg">
+                            <div className="flex flex-col z-10    items-end gap-4 absolute dark:bg-gray-800 bg-white  min-w-[290px] md:w-[450px] right-[-20px] md:right-0 top-0 p-6  rounded-lg">
                                 <button
                                     onClick={() => setShowFilters(false)}
                                     className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 dark:hover:bg-blue-700 text-sm lg:text-xl"
